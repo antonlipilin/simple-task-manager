32250abf5a849bb4668c429a2b66c67151f881c1
Spring Mönkey <antonlipilinwork@gmail.com>
2025-07-04 21:22:23 +0400

    Add controller for login and registration

---
+++ src/main/java/com/example/simple_task_manager/user/UserController.java
@@ -0,0 +1,29 @@
+package com.example.simple_task_manager.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/tasks")
+    public String showMainPage() {
+        return "tasks";
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String createAccount() {
+        return "redirect:/login?success";
+    }
+}
