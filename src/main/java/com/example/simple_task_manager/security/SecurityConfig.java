479a85f73d8c13601bbffa44b7bb23cc0ccae9f9
Spring Mönkey <antonlipilinwork@gmail.com>
2025-07-04 15:29:48 +0400

    Add security config

---
+++ src/main/java/com/example/simple_task_manager/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.simple_task_manager.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(request -> {
+            request.requestMatchers("/login", "/registration").permitAll()
+                    .requestMatchers("/tasks", "/", "/logout").authenticated()
+                    .anyRequest().denyAll();
+        }).formLogin(form ->
+                        form.loginPage("/login")
+                                .defaultSuccessUrl("/tasks", true))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
