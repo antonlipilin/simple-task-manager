a0d6c636757f94110c25a7cf4b3d900a17deb9f9
Spring Mönkey <antonlipilinwork@gmail.com>
2025-07-05 17:14:23 +0400

    Add POST(/registration) endpoint for user registration

--- src/main/java/com/example/simple_task_manager/user/UserController.java
+++ src/main/java/com/example/simple_task_manager/user/UserController.java
@@ -1,11 +1,23 @@
 package com.example.simple_task_manager.user;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
     public String login() {
@@ -23,12 +35,25 @@ public class UserController {
     }
 
     @GetMapping("/registration")
-    public String showRegistrationForm() {
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String createAccount() {
+    public String createAccount(@ModelAttribute("user") @Valid UserDto dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        try {
+            userService.saveUser(dto);
+        } catch (UserAlreadyExistsException ex) {
+            bindingResult.rejectValue("username", "error.username", "A user with this username already exists");
+            return "registration";
+        }
+
         return "redirect:/login?success";
     }
 }
