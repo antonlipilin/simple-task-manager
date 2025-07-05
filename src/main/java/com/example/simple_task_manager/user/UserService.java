3129c9c26711ddf2cdfa24097b67c7794b3b6199
Spring Mönkey <antonlipilinwork@gmail.com>
2025-07-05 17:11:10 +0400

    Add user service for user-related operations

---
+++ src/main/java/com/example/simple_task_manager/user/UserService.java
@@ -0,0 +1,39 @@
+package com.example.simple_task_manager.user;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final ModelMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, ModelMapper mapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void saveUser(UserDto dto) throws UserAlreadyExistsException {
+        Optional<User> optionalUser = userRepository.findByUsername(dto.getUsername());
+
+        if (optionalUser.isPresent()) {
+            throw new UserAlreadyExistsException("User already exists");
+        }
+
+        User userEntity = convertToEntity(dto);
+        userRepository.save(userEntity);
+    }
+
+    private User convertToEntity(UserDto dto) {
+        User user = mapper.map(dto, User.class);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        return user;
+    }
+}
