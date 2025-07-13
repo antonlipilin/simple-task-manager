058fd416ce7ffd5721c3495927efdb6081a19866
Spring Mönkey <antonlipilinwork@gmail.com>
2025-07-13 23:00:41 +0400

    Add config for serving user images as static resources

---
+++ src/main/java/com/example/simple_task_manager/config/StaticResourceConfig.java
@@ -0,0 +1,15 @@
+package com.example.simple_task_manager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/avatars/**")
+                .addResourceLocations("file:///home/purplewave/Рабочий%20стол/avatars");
+    }
+}
