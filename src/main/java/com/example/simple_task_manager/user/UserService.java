a2cea7c7e1a578283b4af30443bb21e1246cff4c
Spring Mönkey <antonlipilinwork@gmail.com>
2025-07-23 15:41:34 +0400

    Add method for changing user image to UserService

--- src/main/java/com/example/simple_task_manager/user/UserService.java
+++ src/main/java/com/example/simple_task_manager/user/UserService.java
@@ -1,10 +1,16 @@
 package com.example.simple_task_manager.user;
 
+import com.example.simple_task_manager.security.UserDetailsImpl;
+import com.example.simple_task_manager.user.exception.ImageIOException;
+import com.example.simple_task_manager.user.exception.UserAlreadyExistsException;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -12,12 +18,14 @@ public class UserService {
     private final UserRepository userRepository;
     private final ModelMapper mapper;
     private final PasswordEncoder passwordEncoder;
+    private final ImageFileStorageService imageService;
 
     @Autowired
-    public UserService(UserRepository userRepository, ModelMapper mapper, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, ModelMapper mapper, PasswordEncoder passwordEncoder, ImageFileStorageService imageService) {
         this.userRepository = userRepository;
         this.mapper = mapper;
         this.passwordEncoder = passwordEncoder;
+        this.imageService = imageService;
     }
 
     public void saveUser(UserDto dto) throws UserAlreadyExistsException {
@@ -39,5 +47,44 @@ public class UserService {
 
     public User getUserById(long id) {
         return userRepository.findUserById(id);
+    }
+
+    @Transactional
+    public void changeProfileImage(MultipartFile file, UserDetailsImpl userDetails) throws IOException {
+        String savedFileName = null;
+
+        try {
+            savedFileName = imageService.save(file);
+
+            if (savedFileName != null) {
+                long principalId = userDetails.getUser().getId();
+                User loadedUser = userRepository.findUserById(principalId);
+
+                String oldFileName = loadedUser.getUserPicture();
+                loadedUser.setUserPicture(savedFileName);
+
+                userRepository.save(loadedUser);
+
+                if (oldFileName != null) {
+                    imageService.delete(oldFileName);
+                }
+            }
+
+        } catch (IOException e) {
+
+            if (savedFileName != null) {
+                imageService.delete(savedFileName);
+            }
+
+            throw new ImageIOException("Failed to change profile image. Please try again.", e);
+        }  catch (Exception e) {
+
+            if (savedFileName != null) {
+                imageService.delete(savedFileName);
+            }
+
+            throw e;
+        }
+
     }
 }
